3422bea6457a647b10c3254110aec0e42456f9ed
Gelson <gelson.bertuol@gmail.com>
2022-09-10 17:03:24 -0300

    Service layer, component registration

--- src/main/java/com/example/course/repositories/UserRepository.java
+++ src/main/java/com/example/course/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.course.entities.Usuario;
 
+//@Repository --> Esta annotation é opcional porque a interface já está herdando de JpaRepository que já está registrada como @Repository
 public interface UserRepository extends JpaRepository<Usuario, Long> {
 
 }
