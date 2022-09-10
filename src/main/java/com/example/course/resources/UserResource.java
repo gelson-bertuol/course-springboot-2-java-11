c8d5657a05789706da700c5b5ea21e4876b606de
Gelson <gelson.bertuol@gmail.com>
2022-09-09 22:55:29 -0300

    User entity and resource

---
+++ src/main/java/com/example/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.example.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.course.entities.Usuario;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<Usuario> findAll() {
+		Usuario u = new Usuario(1L, "Maria", "maria@gmail.com", "99999999", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
